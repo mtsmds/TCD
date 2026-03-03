2f217dc314244e3c3214f1e5b7a2da7c01010565
mtsmds <mateus.mendes@ufu.br>
2026-03-03 13:17:40 -0300

    Add files via upload

---
+++ funcoes.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int buscabin (int buscado, int *vet, int tam); // algoritmo de busca binária
+
+int ordenado (int *vet, int tam); // verifica se está ordenado
+
+int* bubblesort(int *vet, int n); //algoritmo de ordenação bubble
