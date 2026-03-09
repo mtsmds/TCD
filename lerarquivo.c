f9156d468dc8e0757562cc9280f30ef82e4d28ef
joseootavio12 <joseotaviozeze95999@gmail.com>
2026-03-09 17:10:06 -0300

    Refactor lerarquivo function to include size parameter

--- lerarquivo.c
+++ lerarquivo.c
@@ -1,17 +1,20 @@
 //Precisa do <stdlib.h>
-void lerarquivo ()
+void lerarquivo (int *V) //obs: se o tamanho é diferente é só mudar no inteiro tam
 {
-        FILE *file = fopen("dados.txt", "r"); //função para ler o arquivo "r" para ler ele
-        if (file == NULL) //verifica se está vazio
+    tam = 100;
+    int n = 0;
+    FILE *file = fopen("dados.txt", "r"); //função para ler o arquivo "r" para ler ele
+    if (file == NULL) //verifica se está vazio
+    {
+        printf("Arquivo Nulo\n");
+    } else {
+        int i = 0;
+        while (fscanf(file, "%d", &V[i]) != EOF && i < tam) //Vai colocar no vetor cada numero EOF para ler os dados até o final do arquivo
         {
-            printf("Arquivo Nulo\n");
-        } else {
-            int i = 0;
-            while (fscanf(file, "%d", &V[i]) != EOF) //Vai colocar no vetor cada numero EOF para ler os dados até o final do arquivo
-            {
-                i++; //incrementa
-            }
-            fclose(file);
-            printf("Arquivo Carregado! %d numeros lidos.\n", i); //avisa que está carregado e o tamanho do vetor
+            i++; //incrementa
         }
+        n = i;
+        fclose(file);
+        printf("Arquivo Carregado! %d numeros lidos.\n", i); //avisa que está carregado e o tamanho do vetor
+    }
 }
