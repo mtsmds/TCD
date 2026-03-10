8d34ecfad1c1e9651c8d177ba6cba0962101c633
joseootavio12 <joseotaviozeze95999@gmail.com>
2026-03-10 19:25:48 -0300

    Add InsertionSort function declaration to funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -12,3 +12,5 @@ int* selectionsort(int *vet, int n); //algoritmo de ordenação selection
 void merge (int *V, int inicio, int meio, int fim); //merge para ordenação e junção do vetor
 
 void MergeSort (int *V, int inicio, int fim); //algoritmo que vai "cortar" o vetor
+
+void InsertionSort (int *V, int N); //algoritmo de ordenação inserion sort
