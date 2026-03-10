e5511b7739b3feea870b45641952c7a9026d83f5
joseootavio12 <joseotaviozeze95999@gmail.com>
2026-03-10 08:23:11 -0300

    Add MergeSort and merge function declarations

--- funcoes.h
+++ funcoes.h
@@ -6,4 +6,9 @@ int ordenado (int *vet, int tam); // verifica se está ordenado
 
 int* bubblesort(int *vet, int n); //algoritmo de ordenação bubble
 
+
 int* selectionsort(int *vet, int n); //algoritmo de ordenação selection
+
+void merge (int *V, int inicio, int meio, int fim); //merge para ordenação e junção do vetor
+
+void MergeSort (int *V, int inicio, int fim); //algoritmo que vai "cortar" o vetor
